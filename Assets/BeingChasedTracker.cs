c4004fb6db72d5428a03d83659363d39eef0ff83
Saicharan Vemuri <vemuri.saicharan@gmail.com>
2026-04-18 12:19:22 -0400

    Add BeingChasedTracker script

---
+++ Assets/BeingChasedTracker.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BeingChasedTracker : MonoBehaviour
+{
+
+    static bool IsBeingChased = false;
+
+    [SerializeField] private FMODUnity.StudioEventEmitter ShallowsMusicEvent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
